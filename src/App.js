25369af074aba5a90fadb3d8b17e8eb41da6f144
strikertanmay <tanmaymittal0@gmail.com>
2019-03-15 04:32:02 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Navbar/>
+          <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    );
+  };
+};
+
+export default App;
