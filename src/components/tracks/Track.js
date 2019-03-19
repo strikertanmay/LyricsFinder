f5b011e07d47811d768a1912b3158f495e101ab3
strikertanmay <tanmaymittal0@gmail.com>
2019-03-19 13:09:38 +0530

    update in Track.js

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -6,12 +6,12 @@ const Track = (props) => {
     <div className="col-md-6">
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
-        <h5>{track.artist_name}</h5>
-        <p className=
+          <h5>{track.artist_name}</h5>
+          <p className = "card-text"></p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
  
 export default Track;
